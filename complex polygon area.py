2d206cb23a6410cc2bace67d0cdc34c39b9617fd
jex <72693376+ministove3yo@users.noreply.github.com>
2020-12-31 14:45:46 +0800

    Update complex polygon area.py

--- complex polygon area.py
+++ complex polygon area.py
@@ -1,14 +1,35 @@
-from math import *
+import math
 from typing import List
+import numpy as np
+vertex_number = int(input('number of vertex: '))
+vertex = np.array([list(map(int, input(f'vertex {i+1}: ').split())) for i in range(vertex_number)])
 
-vertex_number = int(input())
-vertex = [list(map(int, input().split())) for _ in range(vertex_number)]
-center = [100001, 100001]
+for i in range(0, round(1e10)):
+    avg = round(np.sum(vertex[:, 0]) / vertex_number)
+    if not avg - i in vertex[:, 0]:
+        x = avg - i
+        break
+    elif not avg + i in vertex[:, 0]:
+        x = avg + i
+        break
+
+for i in range(0, round(1e10)):
+    round((np.max(vertex[:, 1]) + np.min(vertex[:, 1])) / 2)
+    avg = round(np.sum(vertex[:, 1]) / vertex_number)
+    if not avg - i in vertex[:, 0]:
+        y = avg - i
+        break
+    elif not avg + i in vertex[:, 0]:
+        y = avg + i
+        break
+
+
+center = [x, y]
 f_area = 0
 
+
 def GetAngle(a, b, c):
-    ang = degrees(atan2(c[1] - b[1], c[0] - b[0]) - atan2(a[1] - b[1], a[0] - b[0]))
-    assert ang >= 0 and ang <= 360
+    ang = math.degrees(math.atan2(c[1] - b[1], c[0] - b[0]) - math.atan2(a[1] - b[1], a[0] - b[0]))
     return ang + 360 if ang < 0 else ang
 
 
@@ -16,11 +37,11 @@ def triangle(t1, t2, t3):
     x1, y1 = t1[0], t1[1]
     x2, y2 = t2[0], t2[1]
     x3, y3 = t3[0], t3[1]
-    a = hypot(x1 - x2, y1 - y2)
-    b = hypot(x2 - x3, y2 - y3)
-    c = hypot(x3 - x1, y3 - y1)
+    a = math.hypot(x1 - x2, y1 - y2)
+    b = math.hypot(x2 - x3, y2 - y3)
+    c = math.hypot(x3 - x1, y3 - y1)
     p = (a + b + c) / 2
-    return round(sqrt(p * (p - a) * (p - b) * (p - c)), 4)
+    return round(math.sqrt(p * (p - a) * (p - b) * (p - c)), 4)
 
 
 def add_or_not(n):
@@ -83,4 +104,4 @@ triangle_add = [add_or_not(i) for i in range(vertex_number)]
 triangle_areas = [triangle(center, vertex[j], vertex[(j + 1) % vertex_number]) for j in range(vertex_number)]
 for p, a in zip(triangle_add,triangle_areas):
     f_area = eval(f"{f_area}{'+'if p else'-'}{a}")
-print(round(f_area, 1))
+print(abs(f_area))
